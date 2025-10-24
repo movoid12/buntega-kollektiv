b95a65388ad118d607e3c48d0edc89978cc415be
Mouaz Aldakkak <87415713+movoid12@users.noreply.github.com>
2025-10-24 18:11:39 +0000

    feat: add missing pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 export const metadata: Metadata = {
   title: "Buntega Kollektiv",
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body>
         <Header />
         <div className="min-h-screen bg-white">{children}</div>
+        <Footer />
       </body>
     </html>
   );
