ec1d9686840ecc4030baa394b8258e3933a7133f
Mouaz Aldakkak <87415713+movoid12@users.noreply.github.com>
2026-01-03 13:16:27 +0000

    refactor(tailwind): migrate tailwindcss to v4

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -5,11 +5,11 @@ import { cn } from "@/lib/utils"
 import React from "react"
 
 const alertVariants = cva(
-  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
+  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]: ",
   {
     variants: {
       variant: {
-        default: "bg-background text-foreground",
+        default: "   ",
         destructive:
           "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
       },
